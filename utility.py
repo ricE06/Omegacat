0cff4bdf10d47e321435892f55d7a6ee944526c1
ricE06 <94811368+ricE06@users.noreply.github.com>
2024-08-29 17:58:33 -0400

    New feature for students to find classmates

--- utility.py
+++ utility.py
@@ -9,17 +9,39 @@ import asyncio
 import requests
 from table2ascii import table2ascii as t2a, PresetStyle
 from table2ascii import Alignment
+import sqlite3
+
+
+# Connect to the database
+con = sqlite3.connect("courses.db")
+cur = con.cursor()
+# CREATE TABLE courseloads (id INTEGER PRIMARY KEY, name TEXT DEFAULT "");
+# New columns are added for each valid course. There is a command to wipe the database for the new semester.
+
+
+
 
 class Utility(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 		self.time_periods = ("offered_fall", "offered_IAP", "offered_spring", "offered_summer")
-		self.format_tags = {"-d": lambda a: f"```Description: {a.get("description", "No description available")}\nInstructor(s): {", ".join(a.get("instructors", ["No instructors available"]))}```", \
-							"-l": lambda a: f"Link: <{a.get("url", "No link available")}>\n", \
-							"-u": lambda a: f"```{a.get("total_units", "")} total units ({a.get("lecture_units", "")}|{a.get("preparation_units", "")}|{a.get("lab_units", "")}|{a.get("design_units", "")}) (lec|prep|lab|design)```", \
-							"-s": lambda a: f"```{"\n".join(" ".join(a.get("schedule", "No schedule available").split(",")).split(";"))}```", \
-							"-a": lambda a: f"```Available: {", ".join([key.removeprefix("offered_") for key in self.time_periods if a.get(key, False)])}```", \
-							"-p": lambda a: f"```Prerequisites: {a.get("prerequisites", "none")}```"}
+		self.format_tags = {"-d": lambda a: f'```Description: {a.get("description", "No description available")}\nInstructor(s): {", ".join(a.get("instructors", ["No instructors available"]))}```', \
+							"-l": lambda a: f'Link: <{a.get("url", "No link available")}>\n', \
+							"-u": lambda a: f'```{a.get("total_units", "")} total units ({a.get("lecture_units", "")}|{a.get("preparation_units", "")}|{a.get("lab_units", "")}|{a.get("design_units", "")}) (lec|prep|lab|design)```', \
+							"-s": lambda a: '```' + "\n".join(" ".join(a.get("schedule", "No schedule available").split(",")).split(";")) + '```', \
+							"-a": lambda a: f'```Available: {", ".join([key.removeprefix("offered_") for key in self.time_periods if a.get(key, False)])}```', \
+							"-p": lambda a: f'```Prerequisites: {a.get("prerequisites", "none")}```'}
+		# self.format_tags = {}
+
+	def check_blacklist(self, user_id):
+		with open('blacklist.txt', 'r') as f:
+			for line in f:
+				if str(user_id) in line:
+					f.close()
+					return True
+			f.close()
+		return False
+
 
 	def lookup_course(self, search):
 		"""Gets the full JSON of a course from Fireroad. 
@@ -68,6 +90,93 @@ class Utility(commands.Cog):
 		output = output + "```"
 		return output
 
+	def add_class_to_table(self, class_num):
+		"""Adds a new column for the course number in courseloads table. Columns are the exact number of the class, decimal included."""
+		cur.execute(f"ALTER TABLE courseloads ADD '{class_num}' TEXT DEFAULT 'x'")
+		con.commit()
+
+	def get_column_names(self):
+		"""Returns the column names in the courseloads table. Used to check if courses already exist in the table."""
+		res = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('courseloads')")
+		output = [i[0] for i in res.fetchall()]
+		print(output)
+		return output
+
+	def edit_entry(self, user_id, class_num, entry):
+		"""Sets the entry for the associated user_id and class_num to entry. Internal use only, assumes all rows and columns exist."""
+		cur.execute(f"UPDATE courseloads SET '{class_num}' = '{entry}' WHERE id = {user_id}")
+		con.commit()
+
+	def modify_courseload(self, user_id, class_tags, name = None):
+		"""Changes the classes associated with a user id, and creates a new row if one does not exist already. 
+		Class tags are a nested tuple, where the first entry is the class is the course id and the second entry is the data entry.
+		The class number should be used to indicate someone is taking the class, and 'x' if not. Extra text can be used to provide more information (e.g. first or second quarter)."""
+		if name is None:
+			name = "unknown"
+		cur.execute(f"INSERT OR IGNORE INTO courseloads(id, name) VALUES ({user_id}, '{name}')")
+		con.commit()
+		existing_classes = self.get_column_names()
+		for class_num, entry in class_tags:
+			if class_num not in existing_classes:
+				self.add_class_to_table(class_num)
+			self.edit_entry(user_id, class_num, entry)
+
+	def get_classes_taken(self, user_id):
+		"""Returns a list of the internal name followed by all the classes that the user is taking (i.e. nondefault entries)."""
+		res = cur.execute(f"SELECT * FROM courseloads WHERE id = {user_id}")
+		raw = res.fetchone()
+		if raw is None:
+			return ["Not a valid ID!"]
+		output = [raw[1]]
+		for entry in raw[2:]:
+			if entry != "x":
+				output.append(entry)
+		return output
+
+	def get_taking_class(self, class_num):
+		"""Returns a list of tuples (user_id, name) taking a certain class (i.e. nondefault entries). Returns the empty list if no class exists."""
+		if class_num not in self.get_column_names():
+			return []
+		res = cur.execute(f"SELECT id, name, {class_num} FROM courseloads")
+		raw = res.fetchall()
+		print(raw)
+		output = []
+		for entry in raw:
+			if entry[2] != "x":
+				output.append(entry[1:])
+		return output
+
+	def sanitize_name(self, path):
+		path = path[0:30]
+		length = len(path)
+		SAFE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.|/ "
+		safepath = ""
+		for i in range(length):
+			if path[i] in SAFE:
+				safepath = safepath + path[i]
+		return safepath
+
+	def process_tags(self, tags):
+		"""Returns a list of class_tags suitable for modify_courseload(). Input should be a list of strings. Only considers ones beginning with + or -."""
+		output = []
+		mapping = {"+": lambda a: a, "-": lambda a: "x"}
+		all_add = True
+		for tag in tags:
+			if tag[0] == "+" or tag[0] == "-":
+				all_add = False
+				break
+		for tag in tags:
+			if tag[0] == "+" or tag[0] == "-" or all_add:
+				if all_add:
+					operator = "+"
+				else:
+					operator = tag[0]
+					tag = tag[1:]
+				search_result = self.lookup_course(tag)
+				if type(search_result) is not int and len(search_result) > 0:
+					class_num = search_result.get("subject_id")
+					output.append((class_num, mapping[operator](class_num)))
+		return output
 
 	@commands.command(name="lookup", aliases=["search", "find", "course"])
 	async def lookup(self, ctx, search, *args):
@@ -104,6 +213,47 @@ class Utility(commands.Cog):
 			return
 		await ctx.send(self.format_search_results(search, search_lookup))
 
+	@commands.command(name="add_courseload", aliases=["add_courses", "addc"])
+	async def add_courseload(self, ctx, *args):
+		"""Command to add courses to the lookup table. Begin each class with either '+' or '-' to add or remove a class, followed by the class number.
+		Separate classes by spaces."""
+		if self.check_blacklist(ctx.author.id):
+			return
+		if args is None:
+			await ctx.send("You must input at least one class!")
+			return
+		tags = self.process_tags(args)
+		self.modify_courseload(ctx.author.id, tags, self.sanitize_name(ctx.author.display_name))
+		await ctx.send(f"{len(tags)} classes successfully updated.")
+
+	@commands.command(name="see_courseload", aliases=["see_courses", "seec"])
+	async def see_courseload(self, ctx, search = None):
+		"""View who is taking your class, or which classes one is taking. Without parameters, returns the classes you are taking. If you input a user id or ping 
+		another user, it returns the class they are taking. If you input a course number, it returns everyone who has indicated they are taking the class.""" 
+		if self.check_blacklist(ctx.author.id):
+			return
+		if search is None:
+			search = ctx.author.id 
+		if strip(str(search)) != -1:
+			user_id = strip(str(search))
+			await ctx.send(" ".join(self.get_classes_taken(user_id)))
+			return
+		taking = self.get_taking_class(search)
+		if len(taking) == 0:
+			await ctx.send("No one is taking that class!")
+		else:
+			await ctx.send(", ".join([name for name, entry in taking]))
+
+	@commands.command(name="blacklist")
+	@admin
+	async def blacklist(self, ctx, target):
+		"""Adds a user to the blacklist."""
+		with open('blacklist.txt', 'a') as f:
+			f.write(str(strip(target)) + "\n")
+			f.close()
+		cur.execute(f"DELETE FROM courseloadsk WHERE id = {strip(target)}")
+		con.commit()
+		await ctx.send("User added to blacklist and entries deleted.")
 
 
 async def setup(client):
